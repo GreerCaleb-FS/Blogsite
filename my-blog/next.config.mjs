a7035b6f8f53448659bda599ed0256a30fe5537b
GreerCaleb-FS <cdgreer@student.fullsail.edu>
2025-08-11 15:32:50 -0500

    push

--- my-blog/next.config.mjs
+++ my-blog/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/Blogsite", // repo name
+  assetPrefix: "/Blogsite/",
 };
 
 export default nextConfig;
